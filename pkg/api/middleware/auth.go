a28d3079dc003b4bee6b36b8cfa2922ca15b56d1
Rahul Chacko <rahulchacko7@gmail.com>
2023-12-02 21:21:46 +0530

    admin no token or invalid token error fixed, now message available

--- pkg/api/middleware/auth.go
+++ pkg/api/middleware/auth.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -19,8 +20,11 @@ func AdminAuthMiddleware(c *gin.Context) {
 
 	if err != nil {
 		// The access token is invalid.
-		c.AbortWithStatus(401)
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid authorization token"})
+		c.Abort()
 		return
+		// c.AbortWithStatus(401)
+		// return
 	}
 
 	c.Next()
